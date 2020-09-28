6bce90b2d757a2d9c48a83075f7facefbddba17d
HamiltonGravito <hamiltongravito@gmail.com>
2020-09-28 01:32:21 -0300

    Header Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import { AlbumComponent } from './album/album.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostComponent,
+    AlbumComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
